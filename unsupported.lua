2285213d274829f2a04ba6698e4e243a21f75d3e
plowingsimulatorexe <nathanathan69420@gmail.com>
2025-12-01 19:05:40 +0800

    Update unsupported.lua

--- unsupported.lua
+++ unsupported.lua
@@ -12,7 +12,7 @@ end
 
 local mainWindow = Library:CreateWindow({
     Title = "Plow's\nPrivate Script",
-    Footer = "v1.1.3",
+    Footer = "v1.1.2",
     NotifySide = "Right",
     ShowCustomCursor = true,
 })
