a78a7956ef773465db3b64c7bd25e16f51f4bfac
nathanathan69420 <nathanathan69420@users.noreply.github.com>
2026-01-02 18:55:26 +0800

    Reverted version to v1.7.4

--- possessor_script.lua
+++ possessor_script.lua
@@ -17,7 +17,7 @@ theme.BuiltInThemes["Default"][2] = {
 
 local win = lib:CreateWindow({
     Title = "Axis Hub - Possessor.lua",
-    Footer = "by RwalDev & Plow | 2.7.4 | Discord: .gg/UuyxhqgEVs",
+    Footer = "by RwalDev & Plow | 1.7.4 | Discord: .gg/UuyxhqgEVs",
     NotifySide = "Right",
     ShowCustomCursor = true,
 })
--- violence_district_script.lua
+++ violence_district_script.lua
@@ -87,7 +87,7 @@ theme.BuiltInThemes["Default"][2] = {
 
 local win = lib:CreateWindow({
     Title = "Axis Hub -\nViolence District.lua",
-    Footer = "by RwalDev & Plow | 2.7.4 | Discord: .gg/UuyxhqgEVs",
+    Footer = "by RwalDev & Plow | 1.7.4 | Discord: .gg/UuyxhqgEVs",
     NotifySide = "Right",
     ShowCustomCursor = true,
 })
